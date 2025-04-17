3205598b69badaa2a8babd17a9b1dcc38c0bf7bc
mmoyaa <105326109+mmoyaa@users.noreply.github.com>
2025-04-16 23:21:03 -0400

    Update probando2.component.ts

--- src/app/modulos/probando2/probando2.component.ts
+++ src/app/modulos/probando2/probando2.component.ts
@@ -53,8 +53,72 @@ submit() {
   this.validadorform.reset();
 }
 
+-----------------------------------------------------------------
+
+  movall: any[] = []; // Lista de movimientos
+  movimientosSeleccionados: any[] = []; // Lista de movimientos seleccionados
+  movimientoSeleccionado: FormControl = new FormControl(''); // FormControl para el select
 
 
+  movimientoSeleccionados: FormControl = new FormControl('');
+constructor(private regionesService: RegionesService) { }
+    validadorform = new FormGroup({
+      dato1: new FormControl('', [Validators.required]),
+      dato2: new FormControl('', [Validators.required]),
+      dato3: new FormControl('', [Validators.required]),
+      nombre: new FormControl('', [Validators.required]),
+      apellido: new FormControl('', [Validators.required]),
+      edad: new FormControl('', [Validators.required]),
+      validacion: new FormControl('', [Validators.required])
+    });
+
+
+  
+    // Método para cargar los movimientos desde el backend
+    getmovimientos() {
+      this.regionesService.getmovimientos().subscribe((res: any) => {
+        this.movall = res;
+        console.log(this.movall);
+      });
+    }
+  
+    // Método para agregar el movimiento seleccionado a la grilla
+    agregarMovimiento() {
+      const selectedId = this.movimientoSeleccionado.value;
+      const movimiento = this.movall.find(m => m.id == selectedId);
+  
+      // Evitar duplicados en la grilla
+      const yaExiste = this.movimientosSeleccionados.some(m => m.id == movimiento.id);
+  
+      if (movimiento && !yaExiste) {
+        this.movimientosSeleccionados.push(movimiento);
+      }
+    }
+  
+    // Método para eliminar un movimiento de la grilla
+    eliminarMovimiento(id: number) {
+      this.movimientosSeleccionados = this.movimientosSeleccionados.filter(m => m.id !== id);
+    }
+  
+    // Método para guardar los movimientos en el backend
+    guardarMovimientos() {
+      if (this.movimientosSeleccionados.length === 0) {
+        alert('No hay movimientos para guardar.');
+        return;
+      }
+  
+      this.regionesService.guardarMovimientos(this.movimientosSeleccionados).subscribe({
+        next: (res) => {
+          console.log('Movimientos guardados correctamente:', res);
+          alert('Movimientos guardados exitosamente!');
+          this.movimientosSeleccionados = []; // Limpiar tabla
+        },
+        error: (err) => {
+          console.error('Error al guardar movimientos:', err);
+          alert('Hubo un error al guardar los movimientos.');
+        }
+      });
+    }
 
 
 
